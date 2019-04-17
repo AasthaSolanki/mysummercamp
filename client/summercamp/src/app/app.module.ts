a0a04f26002ac3e42283c71183e139b08fe21b44
AasthaSolanki <aastha.solanki2015@vit.ac.in>
2019-04-17 16:58:19 +0530

    ok

--- client/summercamp/src/app/app.module.ts
+++ client/summercamp/src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import {RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from './components/login/login.component';
 
+const appRoutes: Routes = [
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  {path: 'login', component: LoginComponent}
+  ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    BrowserModule, RouterModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
